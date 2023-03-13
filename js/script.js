7266a6559d992a69995069b191398edb6a45206a
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-03-13 12:44:39 +0100

    message printed

--- js/script.js
+++ js/script.js
@@ -3,16 +3,12 @@ Stampare a schermo un messaggio all’interno di un h1,
 utilizzando i data.
 */ 
 
+const { createApp } = Vue
 
 createApp({
-
     data() {
-
       return {
-
-        
+        title: "Questo è un messaggio stampato in pagina"
       }
     }
-
-
   }).mount('#app')
