6149a4c603e57e89335c536041bccb235aab4bd2
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-03-13 13:00:35 +0100

    bonus done excercise completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 /*
 Stampare a schermo un messaggio all’interno di un h1,
 utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */ 
 
 const { createApp } = Vue
@@ -8,7 +11,8 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        title: "Questo è un messaggio stampato in pagina"
+        title: "Questo è un messaggio stampato in pagina",
+        image: "img/logo.png"
       }
     }
   }).mount('#app')
