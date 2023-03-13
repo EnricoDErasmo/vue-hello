abf46a2f6aa93a3fbe0d3d64a32a1c8e1a389d9e
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-03-13 12:26:35 +0100

    scaffholding

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+Stampare a schermo un messaggio all’interno di un h1,
+utilizzando i data.
+*/ 
+
+
+createApp({
+
+    data() {
+
+      return {
+
+        
+      }
+    }
+
+
+  }).mount('#app')
